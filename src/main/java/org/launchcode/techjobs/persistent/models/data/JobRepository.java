76a1876733be771b3e56fdf4fcdd9ed8236ed18d
Mitchell Johnson <mitchell.johnson624@gmail.com>
2022-10-17 13:59:00 -0400

    Assignment 4

--- src/main/java/org/launchcode/techjobs/persistent/models/data/JobRepository.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/JobRepository.java
@@ -1,10 +1,2 @@
-package org.launchcode.techjobs.persistent.models.data;
-
-import org.launchcode.techjobs.persistent.models.Employer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployerRepository<Employer> extends  CrudRepsitory<Employer, Integer> {
-
+package org.launchcode.techjobs.persistent.models.data;public interface JobRepository {
 }
